9ace83693be5ab2c6a68e99f3a15ea93e3eb4c10
eesfernandes <eesf@hotmail.com>
2023-02-10 10:23:09 -0300

    feedback-strings-exercicios

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -4,7 +4,7 @@ const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      
 // a) - Removendo espaço no final da frase com o metodo trim()
 const removeEspaco = minhaString.trim();
 
-// b) - Texto antes de depois de manipular as string's
+// b) - Texto antes e depois de manipular a string
 console.log(`Total de caracteres no texto original: ${minhaString.length}
 Total de caracteres removendo espaços no final do texo: ${removeEspaco.length}`);
 
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,14 +1,21 @@
 // a) - Crie a const para a frase aqui
 const minhaFrase = (`Jorge tem uma casa verde e com portão azul, com os dizeres: \"BOAS VINDAS, mas não deixe o gato sair\"`);
+console.log(minhaFrase)
+console.log(`-------------------------------------------------------------`)
 
 // b) - Trocando verde por rosa e azul por laranja com o metodo replace()
 const trocarCores = minhaFrase.replace('verde', 'rosa').replace('azul', 'laranja');
+console.log(trocarCores)
+console.log(`-------------------------------------------------------------`)
 
 // c) - Verifica se as alterações de cores foram feitas (obs: exercicio menciona erroneamente verde ao inves de rosa)
-console.log(`Frase original: ${minhaFrase}
+const incluiVerde = trocarCores.includes('verde')
+console.log(`Inclui verde: ${incluiVerde}`)
 
-Frase modificada: ${trocarCores}`);
+const incluiLaranja = trocarCores.includes('laranja')
+console.log(`Inclui laranja: ${incluiLaranja}`)
+console.log(`-------------------------------------------------------------`)
 
 // Extra) - Fazendo a palavra "mas não deixe o gato sair" ficar em maiúsco
-const fraseMaiusc = trocarCores.replaceAll('mas não deixe o gato sair','mas não deixe o gato sair'.toUpperCase())
+const fraseMaiusc = trocarCores.replace('mas não deixe o gato sair','mas não deixe o gato sair'.toUpperCase())
 console.log(fraseMaiusc)
